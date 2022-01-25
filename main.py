b23136f584d293cfe757dc76abb8751c3eb2236b
Jovan <savkovic.jovan@protonmail.com>
2022-01-25 13:59:00 +0100

    meta levo desno

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ shuriken_image = pygame.image.load('images/shuriken.png')
 shuriken_image = pygame.transform.smoothscale(shuriken_image, (30, 30))
 
 entity_list = []
-entity_list.append(entities.Square(top_left, top_right, bottom_left, bottom_right, [(0,0)], screen))
+entity_list.append(entities.Square(top_left, top_right, bottom_left, bottom_right, [((5, 0), 30), ((-5,0), 30)], screen))
 shuriken = entities.Shuriken(400, 600, (0, 0), screen)
 ball = entities.Ball(400, 600, 10, (0, 0), screen)
 clock = pygame.time.Clock()
